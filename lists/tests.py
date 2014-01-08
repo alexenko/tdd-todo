3d3b51f7dffb4bc9f928f7350fe9f0027f7260ed
Alexander Gavrilenko <alexenko@gmail.com>
2014-01-08 00:08:25 -0500

    Refactor: split homepage test save/redirect test into two tests

--- lists/tests.py
+++ lists/tests.py
@@ -28,6 +28,13 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.all().count(), 1)
         new_item = Item.objects.all()[0]
         self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
         
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
