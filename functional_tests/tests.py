6777f4d5b5410a0c99fc756d1926ebcd19ebab7f
Alexander Gavrilenko <alexenko@gmail.com>
2014-01-08 21:09:49 -0500

    new URL + view for adding to exsiting lists. FT passes :D

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -55,8 +55,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now show both items on her list
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Now a new user, Francis, comes along to the site.
         self.browser.quit()
