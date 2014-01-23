ec639e934153201e6733869d91b64c825b6374fb
Alexander Gavrilenko <alexenko@gmail.com>
2014-01-23 01:22:32 -0500

    FTs take screenshots and dump html

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 from django.test import LiveServerTestCase
 
 from selenium import webdriver
 import sys
+import os
+
+SCREEN_DUMP_LOCATION = os.path.abspath(os.path.join(os.path.dirname(__file__), 'screendumps'))
 
 
 class FunctionalTest(LiveServerTestCase):
@@ -26,7 +30,36 @@ class FunctionalTest(LiveServerTestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
+        if not self._outcomeForDoCleanups.success:
+            if not os.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '*.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', ',')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
